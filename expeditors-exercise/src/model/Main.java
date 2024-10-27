8fd74be45f9042f0da50e453520c3252c8069e36
JJ Coldiron <jj.coldiron@outlook.com>
2024-10-27 15:02:24 -0700

    Added project skeleton

---
+++ expeditors-exercise/src/model/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Expeditors Coding Exercise
+ * JJ Coldiron
+ */
+
+package model;
+
+/**
+ * Driver class for program. Handles file input/output
+ * and core logic.
+ *
+ * @author JJ Coldiron (jj.coldiron@outlook.com)
+ * @version 27 Oct 2024
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // execution begins
+    }
+
+}
---
+++ expeditors-exercise/src/model/Occupant.java
@@ -0,0 +1,15 @@
+/*
+ * Expeditors Coding Exercise
+ * JJ Coldiron
+ */
+
+package model;
+
+/**
+ *
+ * @author JJ Coldiron (jj.coldiron@outlook.com)
+ * @version 27 Oct 2024
+ */
+public class Occupant {
+    
+}
